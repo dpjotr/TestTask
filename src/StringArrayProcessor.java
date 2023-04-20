013abb0cee8d6aa0e3d71e447201717679d1e316
pjotr <danilovpjotr@gmail.com>
2023-04-20 12:27:05 +0300

    Adds additional class Anagramma which allows significantly improve readability

--- src/StringArrayProcessor.java
+++ src/StringArrayProcessor.java
@@ -4,38 +4,35 @@ import java.util.*;
 public class StringArrayProcessor {
     public static void process(String[] source){
 
-        // In this map Keys are Strings containing sorted characters of Strings from source
-        // array and Values are Lists containing counter of strings with the same character
-        // sets in the first position and its indexes of source array in all other positions.
-        Map<String, ArrayList<Integer>> countedSortedStrings = new HashMap<>();
+        Map<String, Anagramma> countedSortedStrings = new HashMap<>();
         for(int i = 0; i < source.length; i++){
             var chars = source[i].toCharArray();
             Arrays.sort(chars);
             String sortedString = new String(chars);
             if (!countedSortedStrings.containsKey(sortedString)){
-                ArrayList<Integer> value = new ArrayList<>();
-                value.add(1);
-                value.add(i);
-                countedSortedStrings.put(sortedString, value);
+                Anagramma anagramma = new Anagramma(i);
+                countedSortedStrings.put(sortedString, anagramma);
             }
             else{
-                List<Integer> newValue = countedSortedStrings.get(sortedString);
-                newValue.set(0, newValue.get(0) + 1);
-                newValue.add(i);
+                countedSortedStrings.get(sortedString).counter++;
+                countedSortedStrings.get(sortedString).positions.append(", ").append(i);
             }
         }
         for (var keyValue: countedSortedStrings.entrySet()){
-            List<Integer> values = keyValue.getValue();
 
             // Print strings with same characters
-            if (values.get(0) > 1){
-                StringBuilder toPrint = new StringBuilder(keyValue.getKey() + "= ");
-                toPrint.append(values.get(1));
-                for(int i = 2; i < values.size(); i++){
-                    toPrint.append(", ").append(values.get(i));
-                }
-                System.out.println(toPrint);
+            if (keyValue.getValue().counter > 1){
+                System.out.println(
+                        keyValue.getValue()
+                                .positions.insert(0, keyValue.getKey() + "= "));
             }
         }
+    }
+}
+class Anagramma{
+    int counter = 1;
+    StringBuilder positions = new StringBuilder();
+    Anagramma(int position){
+        positions.append(position);
     }
 }
